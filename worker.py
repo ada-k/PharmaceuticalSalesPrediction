eeda086d55f113a6ecb454049b4b003870753df4
ada <adakibetj@protonmail.com>
2020-08-22 20:27:28 +0300

    update worker file

--- worker.py
+++ worker.py
@@ -1,9 +1,15 @@
+""" Worker dyno set up. Allows for heavy processes to run in background
+Only works if you've subscribed to standard plan on heroku. The free subscription limits 
+to only one dyno which is the web"""
+
+# Libraries + imports
 import os
 from worker import conn
 import redis
 from rq import Worker, Queue, Connection
 from src.pages.pred import write, 
 
+# create connection
 listen = ['high', 'default', 'low']
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 conn = redis.from_url(redis_url)
@@ -15,5 +21,5 @@ if __name__ == '__main__':
         worker.work()
 
 
-
+# make a request for the pred script.
 result = q.enqueue(write, 'http://heroku.com')
